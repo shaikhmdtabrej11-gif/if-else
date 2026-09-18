9c1fabe320b43301402a06ad59f265d692f1236b
shaikhmdtabrej11-gif <shaikhmdtabrej11@gmail.com>
2026-09-18 23:32:54 +0530

    vs code

---
+++ demo.java
@@ -0,0 +1,48 @@
+public class demo {
+    public static void main(String[] args) {
+        // boolean b = true;
+        // if (b == true) {
+        //     System.out.println("b is true");
+        // }
+        // else{
+        // System.out.println("b is not true");
+// int i = 10;
+// if(i % 2 == 0){
+//     System.out.println("i is even");
+// }
+//     else{
+//         System.out.println("i is odd");
+
+  //  }
+
+   // }
+//    int i = 9;
+//    if (i == 5) {
+//        System.out.println("i is 5");
+
+//    }
+//    else if(i == 6) {
+//     System.out.println("i is 6");
+//    }
+//    else if(i == 7) {
+//     System.out.println("i is 7");
+//    }
+int age = 15;
+if(age > 80) {
+System.out.println("you are very old");
+}
+else if(age > 40) {
+    System.out.println("you are old");
+}
+    else if(age > 30) {
+        System.out.println("you are young");
+
+}
+else if(age > 20) {
+    System.out.println("you are children");
+}
+else{
+    System.out.println("you are new born baby");
+}
+}
+}
